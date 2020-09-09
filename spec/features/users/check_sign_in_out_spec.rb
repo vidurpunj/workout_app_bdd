fc676df0e0ec4db49bedaaa640ef03a94c6d6cc5
vidur.tukatech <vidurp@tukatech.com>
2020-09-09 18:14:02 +0530

    Master Update

---
+++ spec/features/users/check_sign_in_out_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Check'do
+  before do
+    @email = Faker::Internet.email
+    @password = [*('a'..'z'), *('A'..'Z'), *(0..9)].sample(8).join
+    User.create({email: @email, password: @password})
+  end
+
+  scenario 'check logout link' do
+    visit '/'
+    click_link 'Login'
+    fill_in 'Email', with: @email
+    fill_in 'Password', with: @password
+    click_button 'Log in'
+    expect(page).to have_link('Logout')
+  end
+
+  scenario 'check Login link' do
+    visit '/'
+    expect(page).to have_link('Login')
+    expect(page).to have_link('Sign Up')
+  end
+
+end
