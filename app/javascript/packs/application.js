fe3913b37092d97350bb1b02b25af8dc0ac0555d
vidur.tukatech <vidurp@tukatech.com>
2020-09-09 18:05:27 +0530

    Master update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import '../stylesheets/application.scss';
+import '../stylesheets/devise_messages.scss';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
